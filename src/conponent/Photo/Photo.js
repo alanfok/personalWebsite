458ad846351e244af1e8060ee7af140466267940
alan <alan606@gmail.com>
2019-05-03 10:26:34 -0400

    addphoto

--- src/conponent/Photo/Photo.js
+++ src/conponent/Photo/Photo.js
@@ -4,14 +4,18 @@ import { Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Button} from 'reactstrap';
 
 
+ import photo from "../../photo/personalSqr.jpg"
+
+
 class Photo extends Component {
 
     render() {
 
         return (
             <div>
+              <img className="photo" src={photo} alt="Card image cap"></img>
             <Card className="mar">
-              <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+              <CardImg   />
               <CardBody>
                 <CardTitle>Alan Fok</CardTitle>
                 
