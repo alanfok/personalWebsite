ece27205ae14873e5e2c67c0a3c07d6d26fbb1f2
alan <alan606@gmail.com>
2019-08-19 22:21:39 -0400

    update

--- src/conponent/Photo/Photo.js
+++ src/conponent/Photo/Photo.js
@@ -31,8 +31,8 @@ class Photo extends Component {
               <br/>
               <b data-aos="fade-up">Alan Fok</b>
               <p data-aos="fade-up">Hi.I'm the second year Concordia Computer Science student
-                . And want to be a Fullstack developper.</p>
-              <p data-aos="fade-up">I've always had a passion for technology since I was young, even before becoming a Computer Science student</p>
+                . And want to be a Full stack developer.</p>
+              <p data-aos="fade-up">I've always had a passion for technology since I was young, even before becoming a Computer Science student. To become a Full stack developer. I learnt React and Node.JS by myself. and I also learned Java and C# in school and internship.</p>
               <div data-aos="fade-up">
               <button onClick={this.onCVClick} className={"photoButton"}>CV</button>
               </div>
